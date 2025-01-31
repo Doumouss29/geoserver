79f997a3a08cf1d25e46f02b5f3dc8c4989fe374
Gabriel Roldan <gabriel.roldan@camptocamp.com>
2025-01-31 13:12:15 -0300

    [GEOS-11696] fix org.geoserver.sldservice.rest.RasterizerTest
    `org.geoserver.sldservice.rest.RasterizerTest` lost the logged in user
    set up at by the base class `CatalogRESTTestSupport`.
    
    `getAsServletResponse(uri)` requests weren't failing per-se, as they
    returned 200, but the layer wasn't found in `RasterizerController`.
    
    `getAsDOM()` requests failed at parsing the response, but the underlying
    reason was the same, the layer is not found since `AdminRequest` is set
    but the user was `null`.
    
    This patch removes the unnecessary override and refresh of the spring
    application context, hence fixing the root cause of the problem.

--- src/extension/sldService/src/test/java/org/geoserver/sldservice/rest/RasterizerTest.java
+++ src/extension/sldService/src/test/java/org/geoserver/sldservice/rest/RasterizerTest.java
@@ -50,7 +50,6 @@ public class RasterizerTest extends SLDServiceBaseTest {
         final String restPath = RestBaseController.ROOT_PATH + "/sldservice/wcs:World/" + getServiceUrl() + ".xml";
         MockHttpServletResponse response = getAsServletResponse(restPath);
         assertEquals(200, response.getStatus());
-        login(); // re-login, AdminRequest's gone
         Document dom = getAsDOM(restPath, 200);
         ByteArrayOutputStream baos = new ByteArrayOutputStream();
         print(dom, baos);
@@ -65,7 +64,6 @@ public class RasterizerTest extends SLDServiceBaseTest {
         final String restPath = RestBaseController.ROOT_PATH + "/sldservice/wcs:World/" + getServiceUrl() + ".xml";
         MockHttpServletResponse response = getAsServletResponse(restPath);
         assertEquals(200, response.getStatus());
-        login(); // re-login, AdminRequest's gone
         Document dom = getAsDOM(restPath, 200);
         ByteArrayOutputStream baos = new ByteArrayOutputStream();
         print(dom, baos);
@@ -86,7 +84,6 @@ public class RasterizerTest extends SLDServiceBaseTest {
 
         MockHttpServletResponse response = getAsServletResponse(restPath);
         assertEquals(200, response.getStatus());
-        login(); // re-login, AdminRequest's gone
         Document dom = getAsDOM(restPath, 200);
         ByteArrayOutputStream baos = new ByteArrayOutputStream();
         print(dom, baos);
@@ -110,7 +107,6 @@ public class RasterizerTest extends SLDServiceBaseTest {
                 + "classes=5&min=10.0&max=50.0&digits=2&ramp=custom&startColor=0xFF0000&endColor=0x0000FF";
         MockHttpServletResponse response = getAsServletResponse(restPath);
         assertEquals(200, response.getStatus());
-        login(); // re-login, AdminRequest's gone
         Document dom = getAsDOM(restPath, 200);
         ByteArrayOutputStream baos = new ByteArrayOutputStream();
         print(dom, baos);
